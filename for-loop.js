e1e54cbcb1174e3811b346202d6b6393991b39e7
garaddel <garaddel@gmail.com>
2023-05-16 03:08:57 -0400

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,8 @@
+let total = 0
+const limit = 10
+
+for (let i = 1; i < limit; i++) {
+total += i; // total = total + i
+}
+
+console.log(total)
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+roundUp = 2
+rounded = Math.round(1.5)
+console.log(roundUp);
